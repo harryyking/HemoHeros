9161b19eec8d48f56ff0481cd024c6fed78525bb
harryyking <arthurharry06@gmail.com>
2025-01-29 02:23:45 +0000

    width c

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -6,9 +6,9 @@ const Hero = () => {
     <div className='min-h-screen p-4'>
 
     <Nav/>
-    <div className="hero bg-base-200 min-h-screen">
+    <div className="hero min-h-screen">
   <div className="hero-content text-center">
-    <div className="max-w-md">
+    <div className="max-w-lg">
       <h1 className="text-5xl font-bold">Become a Blood & Genotype Expert in Minutes!</h1>
       <p className="py-6">
       Engage with exciting challenges, match blood types, and explore genotypes in a way you've never experienced before.

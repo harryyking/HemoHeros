621d564d9a009781d142a0f89134fd46cc28fcfc
harryyking <arthurharry06@gmail.com>
2025-01-29 20:50:01 +0000

    hero edit

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,9 @@ import Link from "next/link"
 export default function Home() {
 
   return(
-    <div className="hero bg-base-200 min-h-screen">
+    <div className="hero min-h-screen">
   <div className="hero-content text-center">
-    <div className="max-w-md">
+    <div className="max-w-3xl">
       <h1 className="lg:text-6xl text-4xl font-bold">Track Your Cycle, Embrace Your Wellness</h1>
       <p className="py-6">
       Simple, Private, and Insightful - Understand Your Body's Rhythms with Ease

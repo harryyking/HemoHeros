2f2382b98fcb259d02b152da267f016b09ec04ea
harryyking <arthurharry06@gmail.com>
2025-01-29 01:16:15 +0000

    first commit

---
+++ components/Hero.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <section className="relative overflow-hidden bg-blue-50 py-16 sm:py-24">
+    <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="lg:grid lg:grid-cols-2 lg:gap-8 items-center">
+        <div className="max-w-md mx-auto lg:max-w-none lg:mx-0">
+          <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl md:text-6xl">
+          Become a Blood & Genotype Expert in Minutes!
+          </h1>
+          <p className="mt-6 text-xl text-gray-500">
+          Engage with exciting challenges, match blood types, and explore genotypes in a way you've never experienced before.
+          </p>
+          <div className="mt-8">
+            {/* CTA */}
+            
+          </div>
+        </div>
+        <div className="mt-12 lg:mt-0">
+        {/* Interaction */}
+        </div>
+      </div>
+    </div>
+  </section>
+  )
+}
+
+export default Hero
---
+++ components/Nav.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <div className="navbar bg-base-100 max-w-md">
+  <div className="navbar-start tooltip" data-tip="HemoHeros">
+    <a href="" className='btn btn-ghost'>💉</a>
+  </div>
+
+  <div className='navbar-center'>
+    <a className="btn btn-ghost text-xl">HemoHeros</a>
+  </div>
+
+  
+  <div className="navbar-end">
+    <div className='tooltip' data-tip="Learn">
+    <a className="btn">📕</a>
+    </div>
+
+  </div>
+</div>
+  )
+}
+
+export default Nav

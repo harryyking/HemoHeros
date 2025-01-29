6ba7a26f836a27ade2c705101a6bde6e08606f67
harryyking <arthurharry06@gmail.com>
2025-01-29 02:34:20 +0000

    n h adding

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -8,7 +8,7 @@ const Hero = () => {
     <Nav/>
     <div className="hero min-h-screen">
   <div className="hero-content text-center">
-    <div className="max-w-2xl space-y-6">
+    <div className="max-w-2xl space-y-10">
       <h1 className="lg:text-5xl text-3xl font-bold">Become a Blood & Genotype Expert in Minutes!</h1>
       <p>
       Engage with exciting challenges, match blood types, and explore genotypes in a way you've never experienced before.
--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import { BookOpen } from 'lucide-react'
 
 const Nav = () => {
   return (
-    <div className="navbar bg-base-200 max-w-xl mx-auto rounded-md">
+    <div className="navbar bg-base-200 max-w-2xl mx-auto rounded-md">
   <div className="navbar-start">
     <a href="" className='btn btn-ghost'>💉</a>
   </div>
@@ -13,7 +14,7 @@ const Nav = () => {
 
   
   <div className="navbar-end" >  
-    <a className="btn btn-ghost">📕 Learn</a>
+    <a className="btn btn-ghost"><BookOpen className='w-5 h-5'/></a>
 
   </div>
 </div>

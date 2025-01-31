aaa543130732e40f4b45fa303976e935e76879a4
harryyking <arthurharry06@gmail.com>
2025-01-31 22:22:55 +0000

    cta edit u

--- components/cta.tsx
+++ components/cta.tsx
@@ -2,9 +2,9 @@ import { Button } from "@/components/ui/button"
 
 export default function CTA() {
   return (
-      <div className="mx-auto max-w-4xl p-6 space-y-8 rounded bg-primary">
-        <div className="mx-auto max-w-2xl text-center">
-          <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">
+      <div className="mx-auto max-w-4xl p-6 space-y-6 rounded-xl bg-primary">
+        <div className="mx-auto max-w-3xl text-center">
+          <h2 className="text-2xl font-bold tracking-tight text-white sm:text-4xl">
             Are you ready to take your business further?
           </h2>
           <p className="mx-auto mt-6 max-w-xl text-lg text-white leading-8 text-muted-foreground">

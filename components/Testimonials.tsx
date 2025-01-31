85f3cada789ead516dec01bda7ef95af4b153897
harryyking <arthurharry06@gmail.com>
2025-01-31 22:19:28 +0000

    text edit

--- components/Testimonials.tsx
+++ components/Testimonials.tsx
@@ -5,10 +5,10 @@ import { InfiniteMovingCards } from "./ui/inifinite-moving-cards";
 
 export function InfiniteMovingCardsDemo() {
   return (
-    <div className="space-y-6">
-      <h2 className="text-center font-semibold ">What Do Our Customers Say</h2>
+    <div className="space-y-2">
+      <h2 className="text-center font-semibold text-2xl ">What Do Our Customers Say</h2>
 
-    <div className="h-[40rem] rounded-md flex flex-col antialiased bg-white dark:bg-black dark:bg-grid-white/[0.05] items-center justify-center relative overflow-hidden">
+    <div className="h-[30rem] rounded-md flex flex-col antialiased bg-white dark:bg-black dark:bg-grid-white/[0.05] items-center justify-center relative overflow-hidden">
       <InfiniteMovingCards
         items={testimonials}
         direction="right"
--- components/cta.tsx
+++ components/cta.tsx
@@ -2,7 +2,7 @@ import { Button } from "@/components/ui/button"
 
 export default function CTA() {
   return (
-      <div className="mx-auto max-w-4xl p-6 space-y-8 rounded">
+      <div className="mx-auto max-w-4xl p-6 space-y-8 rounded bg-primary">
         <div className="mx-auto max-w-2xl text-center">
           <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">
             Are you ready to take your business further?

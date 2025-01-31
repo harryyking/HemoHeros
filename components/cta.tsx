7df8996853da0b7b0345875c647fd8e2d2ce12ca
harryyking <arthurharry06@gmail.com>
2025-01-31 22:09:42 +0000

    cta edit

--- components/cta.tsx
+++ components/cta.tsx
@@ -2,17 +2,17 @@ import { Button } from "@/components/ui/button"
 
 export default function CTA() {
   return (
-    <div className="py-24 sm:py-32 rounded bg-primary shadow-md px-2">
-      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+    <div className="space-y-10 rounded bg-primary shadow-md">
+      <div className="mx-auto max-w-4xl px-6 lg:px-8">
         <div className="mx-auto max-w-2xl text-center">
           <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">
             Are you ready to take your business further?
           </h2>
-          <p className="mx-auto mt-6 max-w-xl text-lg text-white leading-8 font-muted-background">
+          <p className="mx-auto mt-6 max-w-xl text-lg text-white leading-8 text-muted-foreground">
             Claim Your Free Consultation
           </p>
           <div className="mt-10 flex items-center justify-center gap-x-6">
-            <Button size="lg" variant={"secondary"}>
+            <Button size="lg" variant={"secondary"} className="w-96">
               Book a Call
             </Button>
           </div>
